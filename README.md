ef02f90548312c556bc30df64159204fa382ed15
nirenjan <nirenjan@gmail.com>
2014-05-10 09:49:20 -0700

    Initial version of randomtext plugin

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ octopress-random-text
 =====================
 
 Random text plugin for Octopress/Jekyll
+
+# Description
+This plugin allows you to insert a random line from a text file anywhere in your
+template.
+
+# Installation
+Copy the plugin files to your Octopress blog. Edit the `random-text.html` file
+in `source/_includes/custom/asides` as necessary and add it to your asides list
+as `custom/asides/random-text.html`.
+
+# Usage
+Use the following to pick the default file from /random-texts.txt
+
+    {% randomtext %}
+
+Use the following to pick a specific file.
+
+    {% randomtext file:/data/quotes %}
+
+You can add the above tag in as many places as you want in your template. It
+uses client-side JavaScript to pull a random line from the specified file, so
+every page refresh gets a new random line.
---
+++ plugins/random_text.rb
@@ -0,0 +1,48 @@
+#
+# Author: Nirenjan Krishnan
+#
+# Example usage: {% randomtext %}
+#
+
+module Jekyll
+  class RandomText < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      @filename = nil
+      @id = 'random-text-' + ('0'..'9').to_a.sample(8).join('')
+      
+      # Get an optional filename or use /random-texts.txt
+      if markup =~ /\s*file:(\S+)\s*/i
+        @filename = $1
+      else
+        @filename = '/random-texts.txt'
+      end
+    end
+
+    def render(context)
+      rendered = <<-RANDJS
+        <p id="#{@id}"></p>
+        <script text="javascript">
+          var request = new XMLHttpRequest();
+          request.onload = function() {
+            // get the file contents
+            var fileContent = this.responseText;
+            // split into lines
+            var fileContentLines = fileContent.split( '\\n' );
+            // get a random index (line number)
+            var randomLineIndex = Math.floor( Math.random() * fileContentLines.length );
+            // extract the value
+            var randomLine = fileContentLines[ randomLineIndex ];
+
+            // add the random line in a div
+            document.getElementById( '#{@id}' ).innerHTML = randomLine;
+          };
+          request.open( 'GET', '#{@filename}', true );
+          request.send();
+        </script>
+      RANDJS
+      rendered
+    end
+  end
+end
+
+Liquid::Template.register_tag('randomtext', Jekyll::RandomText)
